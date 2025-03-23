638916ce43224a2b450b2fade914f78739461e9c
Ryan McQuen <rpcm@linux.com>
2025-03-22 19:31:24 -0700

    It is broken.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![Chess Rabbit logo](<assets/Chess Rabbit vintage logo.png>)
 
-Chess Rabbit allows you to drag-and-drop PGN files directly into Inkdrop, and creates a Lichess paste from them, so that you can analyze games, puzzle, or whatever it is you do with chess.
+Chess Rabbit allows you to drag-and-drop PGN files directly into Inkdrop, and creates a Lichess paste from them, so that you can analyze games, puzzles, or whatever it is you do with chess.
 
 Thanks to @k-yagi for the inkdrop-dnd-paste-image plugin for inspiration:
 
---
+++ source/inkdrop_chess_rabbit.ts
@@ -0,0 +1,115 @@
+// import { iframe } from "electron";
+import { markdownRenderer } from "inkdrop";
+
+inkdrop.onEditorLoad(() => {
+    const cm = editor.cm;
+    const mde = inkdrop.getActiveEditor();
+    console.log(mde);
+});
+export default {
+    activate() {
+        // global.inkdrop.onEditorLoad(() => {
+        //     const mde = inkdrop.getActiveEditor();
+        //     inkdrop.commands.add(mde.wrapper.wrapper, {
+        //         "editor:insert-images": (e) => {
+        //             const { files } = e;
+        //             console.log("files dropped:", e);
+
+        //             e.stopPropagation();
+        //         },
+        //     });
+        // });
+        console.log("Chess Rabbit ready to run ...");
+        const mde = inkdrop.getActiveEditor();
+        console.log(mde);
+        if (inkdrop.isEditorActive()) {
+            this.handleEvent.bind(this)(inkdrop.getActiveEditor());
+        } else {
+            global.inkdrop.onEditorLoad(this.handleEvent.bind(this));
+        }
+        if (markdownRenderer) {
+            markdownRenderer.remarkCodeComponents.pgn = pgn;
+            debugger;
+        }
+    },
+
+    deactivate() {
+        inkdrop.getActiveEditor().cm.off("drop", this.functionOnDrop);
+        if (markdownRenderer) {
+            markdownRenderer.remarkCodeComponents.pgn = null;
+        }
+    },
+
+    handleEvent(editor) {
+        const cm = editor.cm;
+        const mde = inkdrop.getActiveEditor();
+        console.log(mde);
+        debugger;
+
+        inkdrop.commands.add(mde.wrapper.wrapper, {
+            "editor:insert-images": (e) => {
+                const { files } = e;
+                console.log("files dropped:", e);
+
+                e.stopPropagation();
+
+                this.functionOnDrop = this.insertPgn.bind(this);
+
+                cm.on("drop", this.functionOnDrop);
+            },
+        });
+    },
+
+    async insertPgn(cm, e) {
+        const pgnContents = e.dataTransfer.getData();
+        console.log(pgnContents);
+        debugger;
+
+        if (!pgnContents) {
+            return false;
+        }
+
+        await this.importPgn(pgnContents);
+        // function (imageArrayBuffer) {
+        //    const imageBuffer = Buffer.from(imageArrayBuffer);
+        //    const imageNativeImage = nativeImage.createFromBuffer(imageBuffer);
+
+        //    if (imageNativeImage.isEmpty()) {
+        //        return false;
+        //    }
+
+        const cursorPosition = cm.getCursor();
+
+        // HACK: When the editor is opened in a separate window, delete inserted URL.
+        cm.replaceSelection("");
+
+        //             inkdrop.commands.dispatch(document.body, "editor:insert-images", {
+        //                 pos: {
+        //                     line: cursorPosition.line,
+        //                     ch: cursorPosition.ch,
+        //                 },
+        //                 files: [imageNativeImage],
+        //             });
+        // }
+    },
+
+    async importPgn(pgnContents) {
+        // const xhr = new XMLHttpRequest();
+        // xhr.onreadystatechange = function () {
+        //     if (xhr.readyState == 4) {
+        //         callback(this.response);
+        //     }
+        // };
+        // xhr.responseType = "arraybuffer";
+        // xhr.open("POST", "https://lichess.org/api/import", {
+        //     pgn: pgnContents,
+        // });
+        // xhr.send();
+
+        await fetch("https://lichess.org/api/import", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams({ pgn: pgnContents }),
+        });
+    },
+};
